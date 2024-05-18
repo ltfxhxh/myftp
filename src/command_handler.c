61097409185539cb673e1dbbf01798d0f085b79a
ltfxhxh <writerxhxh@gmail.com>
2024-05-18 11:50:42 +0800

    log to command_handler.c

--- src/command_handler.c
+++ src/command_handler.c
@@ -1,9 +1,11 @@
 #include "command_handler.h"
 #include "file_operations.h"
+#include "logger.h"
 #include <string.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
 /*========== 处理命令行输入 ==========*/
 void handle_input(int client_fd) {
@@ -16,20 +18,26 @@ void handle_input(int client_fd) {
     numbytes = read(client_fd, buffer, sizeof(buffer));
     if (numbytes < 0) {
         perror("read error");
+        LOG_ERROR("Error reading from client FD=%d: %s", client_fd, strerror(errno));
         exit(EXIT_FAILURE);
     } else if (numbytes == 0) {
+        LOG_INFO("Client disconnected, FD=%d", client_fd);
         printf("Client disconnected\n");
         close(client_fd);
         return;
     }
 
+    LOG_DEBUG("Received command from FD=%d: %s", client_fd, buffer);
     process_command(client_fd, buffer);
 }
 
 /*========== 处理命令行输入的命令 ==========*/
 void process_command(int client_fd, const char *command) {
+    LOG_INFO("Processing command from FD=%d: %s", client_fd, command);
+
     char *args = strdup(command);
     if (!args) {
+        LOG_ERROR("Memory allocation failed when duplicating command string.");
         perror("strdup failed");
         exit(EXIT_FAILURE);
     }
@@ -38,26 +46,36 @@ void process_command(int client_fd, const char *command) {
     char *cmd = strtok_r(args, " \t\r\n", &saveptr);
 
     if (cmd == NULL) {
+        LOG_WARNING("Empty command from FD=%d", client_fd);
         handle_invalid(client_fd);
     } else if (strcmp(cmd, "exit") == 0) {
+        LOG_INFO("Exit command received from FD=%d", client_fd);
         const char *msg = "Exiting, goodbye!\n";
         write(client_fd, msg, strlen(msg));
         close(client_fd);  // Close the connection when "exit" command is received
     } else if (strcmp(cmd, "cd") == 0) {
+        LOG_DEBUG("CD command with path=%s from FD=%d", strtok_r(NULL, " \t\r\n", &saveptr), client_fd);
         handle_cd(client_fd, strtok_r(NULL, " \t\r\n", &saveptr));
     } else if (strcmp(cmd, "ls") == 0) {
+        LOG_DEBUG("LS command received from FD=%d", client_fd);
         handle_ls(client_fd);
     } else if (strcmp(cmd, "puts") == 0) {
+        LOG_DEBUG("PUTS command with filename=%s from FD=%d", strtok_r(NULL, " \t\r\n", &saveptr), client_fd);
         handle_puts(client_fd, strtok_r(NULL, " \t\r\n", &saveptr));
     } else if (strcmp(cmd, "gets") == 0) {
+        LOG_DEBUG("GETS command with filename=%s from FD=%d", strtok_r(NULL, " \t\r\n", &saveptr), client_fd);
         handle_gets(client_fd, strtok_r(NULL, " \t\r\n", &saveptr));
     } else if (strcmp(cmd, "remove") == 0) {
+        LOG_DEBUG("REMOVE command with filename=%s from FD=%d", strtok_r(NULL, " \t\r\n", &saveptr), client_fd);
         handle_remove(client_fd, strtok_r(NULL, " \t\r\n", &saveptr));
     } else if (strcmp(cmd, "pwd") == 0) {
+        LOG_DEBUG("PWD command received from FD=%d", client_fd);
         handle_pwd(client_fd);
     } else if (strcmp(cmd, "mkdir") == 0) {
+        LOG_DEBUG("MKDIR command with dirname=%s from FD=%d", strtok_r(NULL, " \t\r\n", &saveptr), client_fd);
         handle_mkdir(client_fd, strtok_r(NULL, " \t\r\n", &saveptr));
     } else {
+        LOG_WARNING("Invalid command '%s' from FD=%d", cmd, client_fd);
         handle_invalid(client_fd);
     }
 
@@ -66,6 +84,7 @@ void process_command(int client_fd, const char *command) {
 
 /*========== 处理命令行输入的无效命令 ==========*/
 void handle_invalid(int client_fd) {
+    LOG_WARNING("Handling invalid command for FD=%d", client_fd);
     const char *msg = "无效的或不支持的命令\n";
     write(client_fd, msg, strlen(msg));
 }
